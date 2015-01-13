c78982cafce6ae9e9f4f78eb05e1d79279275a54
mada360 <worley9@hotmail.co.uk>
2015-01-13 15:15:17 +0000

    Initial Commit

---
+++ OpenGLCube/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLCube
+//
+//  Created by Adam Worley on 13/01/2015.
+//  Copyright (c) 2015 Adam Worley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
