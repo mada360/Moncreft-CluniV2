d1e781b25a2f25cad5e133aa41874fb1a325a5d6
mada360 <worley9@hotmail.co.uk>
2015-01-16 11:10:04 +0000

    Added more cubes to the scene, rendering issue from the sides of cube.

--- OpenGLCube/Scene.cpp
+++ OpenGLCube/Scene.cpp
@@ -14,7 +14,10 @@
 std::vector<Entity *>* Scene::getChildren(){
     return _children;
 }
-
+//!Scene Creator
+/*!
+ Creates and populates the scene.
+ */
 Scene::Scene(){
     _children = new std::vector<Entity *>();
     
@@ -23,6 +26,21 @@ Scene::Scene(){
                                makeVector3(0.0f, 0.0f, 5.0f));
     
     _children->push_back(entity);
+    
+    Entity *cube2 =new Entity(resourceManager->getVertexBufferArray()->at(1),
+                              makeVector3(1.5f, 0.0f, 5.0f));
+    
+    _children->push_back(cube2);
+    
+    Entity *cube3 =new Entity(resourceManager->getVertexBufferArray()->at(1),
+                              makeVector3(3.0f, 0.0f, 5.0f));
+    
+    _children->push_back(cube3);
+    
+    Entity *cube4 =new Entity(resourceManager->getVertexBufferArray()->at(1),
+                              makeVector3(4.5f, 0.0f, 5.0f));
+    
+    _children->push_back(cube4);
     
     Entity *camera = new Entity(NULL, makeVector3(0.0f, 0.0f, 0.0f));
     camera->setEyeVector(normalizeVector3(makeVector3(0.0f, 0.0f, 1.0f)));
@@ -36,6 +54,10 @@ Scene::Scene(){
     cameraSystem->setCurrentCamera(camera);
 }
 
+//!Scene Destructor
+/*!
+ 
+ */
 Scene::~Scene(){
     for (std::vector<Entity *>::iterator iterator = _children->begin(); iterator != _children->end(); iterator++) {
         delete *iterator;
